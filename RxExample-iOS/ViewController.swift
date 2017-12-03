ed800a9a890b627fef026f3327982624f7e4722e
starhoshi <kensuke1751@gmail.com>
2017-12-03 23:52:20 +0900

    show controllers

--- RxExample-iOS/ViewController.swift
+++ RxExample-iOS/ViewController.swift
@@ -9,17 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    lazy var tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .grouped)
+        view.delegate = self
+        view.dataSource = self
+        return view
+    }()
+
+    let controllers: [UIViewController] = [UIViewController()]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view = tableView
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = controllers[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return controllers.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = String(describing: type(of: controllers[indexPath.row]))
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+}
